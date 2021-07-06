51e9163c02c45e00b38185bb1e255944ba1df061
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-07-06 20:20:11 -0300

    Move variables to their own file.

---
+++ src/services/variables.js
@@ -0,0 +1,116 @@
+export const baseVariables = [
+  // Player 0
+  {
+    variable: 'player0x',
+    icon: 'mdi-account',
+    subIcon: 'mdi-arrow-left-right',
+    color: 'red',
+    name: 'Player 0 X'
+  },
+  {
+    variable: 'player0y',
+    icon: 'mdi-account',
+    subIcon: 'mdi-arrow-up-down',
+    color: 'red',
+    name: 'Player 0 Y'
+  },
+  {
+    variable: 'player0color',
+    icon: 'mdi-account',
+    subIcon: 'mdi-palette',
+    color: 'red',
+    name: 'Player 0 color'
+  },
+
+  // Player 1
+  {
+    variable: 'player1x',
+    icon: 'mdi-account',
+    subIcon: 'mdi-arrow-left-right',
+    color: 'blue',
+    name: 'Player 1 X'
+  },
+  {
+    variable: 'player1y',
+    icon: 'mdi-account',
+    subIcon: 'mdi-arrow-up-down',
+    color: 'blue',
+    name: 'Player 1 Y'
+  },
+  {
+    variable: 'player1color',
+    icon: 'mdi-account',
+    subIcon: 'mdi-palette',
+    color: 'blue',
+    name: 'Player 1 color'
+  },
+
+  // Missile 0
+  {
+    variable: 'missile0x',
+    icon: 'mdi-rocket',
+    subIcon: 'mdi-arrow-left-right',
+    color: 'red',
+    name: 'Missile 0 X'
+  },
+  {
+    variable: 'missile0y',
+    icon: 'mdi-rocket',
+    subIcon: 'mdi-arrow-up-down',
+    color: 'red',
+    name: 'Missile 0 Y'
+  },
+  {
+    variable: 'missile0height',
+    icon: 'mdi-rocket',
+    subIcon: 'mdi-human-male-height',
+    color: 'red',
+    name: 'Missile 0 height'
+  },
+
+  // Missile 1
+  {
+    variable: 'missile1x',
+    icon: 'mdi-rocket',
+    subIcon: 'mdi-arrow-left-right',
+    color: 'blue',
+    name: 'Missile 1 X'
+  },
+  {
+    variable: 'missile1y',
+    icon: 'mdi-rocket',
+    subIcon: 'mdi-arrow-up-down',
+    color: 'blue',
+    name: 'Missile 1 Y'
+  },
+  {
+    variable: 'missile1height',
+    icon: 'mdi-rocket',
+    subIcon: 'mdi-human-male-height',
+    color: 'blue',
+    name: 'Missile 1 height'
+  },
+
+  // Ball
+  {
+    variable: 'ballx',
+    icon: 'mdi-tennis-ball',
+    subIcon: 'mdi-arrow-left-right',
+    color: 'orange',
+    name: 'Ball X'
+  },
+  {
+    variable: 'bally',
+    icon: 'mdi-tennis-ball',
+    subIcon: 'mdi-arrow-up-down',
+    color: 'orange',
+    name: 'Ball Y'
+  },
+  {
+    variable: 'ballheight',
+    icon: 'mdi-tennis-ball',
+    subIcon: 'mdi-human-male-height',
+    color: 'orange',
+    name: 'Ball height'
+  }
+]
