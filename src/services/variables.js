e52855b860ce98f96356696d23a85918db1ee644
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-07-06 20:39:07 -0300

    Refactor variables for easier declaration.

--- src/services/variables.js
+++ src/services/variables.js
@@ -1,116 +1,98 @@
+const assembleDeclarations = (base, items) => items.map(item => ({ ...base, ...item }))
+
 export const baseVariables = [
   // Player 0
-  {
-    variable: 'player0x',
-    icon: 'mdi-account',
-    subIcon: 'mdi-arrow-left-right',
-    color: 'red',
-    name: 'Player 0 X'
-  },
-  {
-    variable: 'player0y',
-    icon: 'mdi-account',
-    subIcon: 'mdi-arrow-up-down',
-    color: 'red',
-    name: 'Player 0 Y'
-  },
-  {
-    variable: 'player0color',
-    icon: 'mdi-account',
-    subIcon: 'mdi-palette',
-    color: 'red',
-    name: 'Player 0 color'
-  },
+  ...assembleDeclarations({ icon: 'mdi-account', color: 'red' }, [
+    {
+      variable: 'player0x',
+      subIcon: 'mdi-arrow-left-right',
+      name: 'Player 0 X'
+    },
+    {
+      variable: 'player0y',
+      subIcon: 'mdi-arrow-up-down',
+      name: 'Player 0 Y'
+    },
+    {
+      variable: 'player0color',
+      subIcon: 'mdi-palette',
+      name: 'Player 0 color'
+    },
+  ]),
 
   // Player 1
-  {
-    variable: 'player1x',
-    icon: 'mdi-account',
-    subIcon: 'mdi-arrow-left-right',
-    color: 'blue',
-    name: 'Player 1 X'
-  },
-  {
-    variable: 'player1y',
-    icon: 'mdi-account',
-    subIcon: 'mdi-arrow-up-down',
-    color: 'blue',
-    name: 'Player 1 Y'
-  },
-  {
-    variable: 'player1color',
-    icon: 'mdi-account',
-    subIcon: 'mdi-palette',
-    color: 'blue',
-    name: 'Player 1 color'
-  },
+  ...assembleDeclarations({ icon: 'mdi-account', color: 'blue' }, [
+    {
+      variable: 'player1x',
+      subIcon: 'mdi-arrow-left-right',
+      name: 'Player 1 X'
+    },
+    {
+      variable: 'player1y',
+      subIcon: 'mdi-arrow-up-down',
+      name: 'Player 1 Y'
+    },
+    {
+      variable: 'player1color',
+      subIcon: 'mdi-palette',
+      name: 'Player 1 color'
+    }
+  ]),
 
   // Missile 0
-  {
-    variable: 'missile0x',
-    icon: 'mdi-rocket',
-    subIcon: 'mdi-arrow-left-right',
-    color: 'red',
-    name: 'Missile 0 X'
-  },
-  {
-    variable: 'missile0y',
-    icon: 'mdi-rocket',
-    subIcon: 'mdi-arrow-up-down',
-    color: 'red',
-    name: 'Missile 0 Y'
-  },
-  {
-    variable: 'missile0height',
-    icon: 'mdi-rocket',
-    subIcon: 'mdi-human-male-height',
-    color: 'red',
-    name: 'Missile 0 height'
-  },
+  ...assembleDeclarations({ icon: 'mdi-rocket', color: 'red' }, [
+    {
+      variable: 'missile0x',
+      subIcon: 'mdi-arrow-left-right',
+      name: 'Missile 0 X'
+    },
+    {
+      variable: 'missile0y',
+      subIcon: 'mdi-arrow-up-down',
+      name: 'Missile 0 Y'
+    },
+    {
+      variable: 'missile0height',
+      subIcon: 'mdi-human-male-height',
+      name: 'Missile 0 height'
+    }
+  ]),
 
   // Missile 1
-  {
-    variable: 'missile1x',
-    icon: 'mdi-rocket',
-    subIcon: 'mdi-arrow-left-right',
-    color: 'blue',
-    name: 'Missile 1 X'
-  },
-  {
-    variable: 'missile1y',
-    icon: 'mdi-rocket',
-    subIcon: 'mdi-arrow-up-down',
-    color: 'blue',
-    name: 'Missile 1 Y'
-  },
-  {
-    variable: 'missile1height',
-    icon: 'mdi-rocket',
-    subIcon: 'mdi-human-male-height',
-    color: 'blue',
-    name: 'Missile 1 height'
-  },
+  ...assembleDeclarations({ icon: 'mdi-rocket', color: 'blue' }, [
+    {
+      variable: 'missile1x',
+      subIcon: 'mdi-arrow-left-right',
+      name: 'Missile 1 X'
+    },
+    {
+      variable: 'missile1y',
+      subIcon: 'mdi-arrow-up-down',
+      name: 'Missile 1 Y'
+    },
+    {
+      variable: 'missile1height',
+      subIcon: 'mdi-human-male-height',
+      name: 'Missile 1 height'
+    }
+  ]),
 
   // Ball
-  {
-    variable: 'ballx',
-    icon: 'mdi-tennis-ball',
-    subIcon: 'mdi-arrow-left-right',
-    color: 'orange',
-    name: 'Ball X'
-  },
-  {
-    variable: 'bally',
-    icon: 'mdi-tennis-ball',
-    subIcon: 'mdi-arrow-up-down',
-    color: 'orange',
-    name: 'Ball Y'
-  },
-  {
-    variable: 'ballheight',
-    icon: 'mdi-tennis-ball',
-    subIcon: 'mdi-human-male-height',
-    color: 'orange',
-    name: 'Ball height'
-  }
+  ...assembleDeclarations({ icon: 'mdi-tennis-ball', color: 'orange' }, [
+    {
+      variable: 'ballx',
+      subIcon: 'mdi-arrow-left-right',
+      name: 'Ball X'
+    },
+    {
+      variable: 'bally',
+      subIcon: 'mdi-arrow-up-down',
+      name: 'Ball Y'
+    },
+    {
+      variable: 'ballheight',
+      subIcon: 'mdi-human-male-height',
+      name: 'Ball height'
+    }
+  ]),
 ]
