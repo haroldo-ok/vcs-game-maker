c91f29651805f655c5cfb17b5e7a6547a1b75713
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-07-06 21:23:09 -0300

    Install `babel-eslint` due to syntax problems.

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,9 @@ module.exports = {
     'Atomics': 'readonly',
     'SharedArrayBuffer': 'readonly',
   },
+  'parser': 'vue-eslint-parser',
   'parserOptions': {
+    'parser': 'babel-eslint',
     'ecmaVersion': 2018,
     'sourceType': 'module',
   },
