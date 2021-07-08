0c1e4f886267b052f980397901f46d5746ecc100
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-07-08 19:57:20 -0300

    Start mocking up screen.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,38 @@
   <v-container>
     <variable-select :items="testItems" v-model="selectedItem" />
     <label>*** {{ selectedItem }} ***</label>
+
+    <v-simple-table>
+        <template v-slot:default>
+          <thead>
+            <tr>
+              <th class="text-left" scope="col">
+                Condition
+              </th>
+              <th class="text-left" scope="col">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <div  class="d-flex flex-row">
+                  <variable-select :items="testItems" />
+                  <v-icon>mdi-equal</v-icon>
+                  <variable-select :items="testItems" />
+                </div>
+              </td>
+              <td class="d-flex flex-row">
+                <div  class="d-flex flex-row">
+                  <v-icon>mdi-arrow-up-bold</v-icon>
+                  <variable-select :items="testItems" />
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </template>
+    </v-simple-table>
   </v-container>
 </template>
 
