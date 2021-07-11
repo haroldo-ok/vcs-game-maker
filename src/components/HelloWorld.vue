668ae2347682c0c0b44ef893cb988385294fdc06
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-07-11 17:59:02 -0300

    Externalize toolbox configuration.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -75,6 +75,7 @@ import VariableSelect from './VariableSelect.vue';
 import BlocklyComponent from './BlocklyComponent.vue';
 
 import '../blocks/stocks';
+import blocklyToolbox from 'raw-loader!./blockly-toolbox.xml';
 import BlocklyJS from 'blockly/javascript';
 
 export default {
@@ -88,52 +89,13 @@ export default {
     code: '',
     options: {
       media: 'media/',
-      grid:
-          {
-            spacing: 25,
-            length: 3,
-            colour: '#ccc',
-            snap: true,
-          },
-      toolbox:
-        `<xml>
-          <category name="Logic" colour="%{BKY_LOGIC_HUE}">
-            <block type="controls_if"></block>
-            <block type="logic_compare"></block>
-            <block type="logic_operation"></block>
-            <block type="logic_negate"></block>
-            <block type="logic_boolean"></block>
-          </category>
-          <category name="Loops" colour="%{BKY_LOOPS_HUE}">
-            <block type="controls_repeat_ext">
-              <value name="TIMES">
-                <block type="math_number">
-                  <field name="NUM">10</field>
-                </block>
-              </value>
-            </block>
-            <block type="controls_whileUntil"></block>
-          </category>
-          <category name="Math" colour="%{BKY_MATH_HUE}">
-            <block type="math_number">
-              <field name="NUM">123</field>
-            </block>
-            <block type="math_arithmetic"></block>
-            <block type="math_single"></block>
-          </category>
-          <category name="Text" colour="%{BKY_TEXTS_HUE}">
-            <block type="text"></block>
-            <block type="text_length"></block>
-            <block type="text_print"></block>
-          </category>
-          <category name="Variables" custom="VARIABLE" colour="%{BKY_VARIABLES_HUE}">
-            </category>
-          <category name="Stocks" colour="%{BKY_LOOPS_HUE}">
-            <block type="stock_buy_simple"></block>
-            <block type="stock_buy_prog"></block>
-            <block type="stock_fetch_price"></block>
-          </category>
-        </xml>`,
+      grid: {
+        spacing: 25,
+        length: 3,
+        colour: '#ccc',
+        snap: true,
+      },
+      toolbox: blocklyToolbox,
     },
   }),
   methods: {
