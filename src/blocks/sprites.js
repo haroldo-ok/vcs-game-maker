3ac454baa1be3e0dd3eeecc005fe4225ed9819b0
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-07-14 21:33:27 -0300

    Create setter block for Player 0.

--- src/blocks/sprites.js
+++ src/blocks/sprites.js
@@ -1,7 +1,7 @@
 import * as Blockly from 'blockly/core';
 
 Blockly.defineBlocksWithJsonArray([ // BEGIN JSON EXTRACT
-  // Block for reading from player 0.
+  // Block for Player 0 getter.
   {
     'type': 'sprite_player0_get',
     'message0': 'Player 0 %1',
@@ -19,5 +19,29 @@ Blockly.defineBlocksWithJsonArray([ // BEGIN JSON EXTRACT
     'output': 'Number',
     'colour': 'red',
     'tooltip': 'Reads information about player zero',
+  },
+  // Block for Player 0 setter.
+  {
+    'type': 'sprite_player0_set',
+    'message0': 'Player 0: %{BKY_VARIABLES_SET}',
+    'args0': [
+      {
+        'type': 'field_dropdown',
+        'name': 'VAR',
+        'options': [
+          ['X', 'player0x'],
+          ['Y', 'player0y'],
+          ['Color', 'player0color'],
+        ],
+      },
+      {
+        'type': 'input_value',
+        'name': 'VALUE',
+      },
+    ],
+    'previousStatement': null,
+    'nextStatement': null,
+    'colour': 'red',
+    'tooltip': 'Updates information about player zero',
   },
 ]);
