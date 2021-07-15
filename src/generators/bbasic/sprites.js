375a9f4d403f85ff77bec8647f3d3db9c0e27f4f
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-07-15 20:07:26 -0300

    Create generators for all sprite blocks.

--- src/generators/bbasic/sprites.js
+++ src/generators/bbasic/sprites.js
@@ -18,28 +18,32 @@ goog.require('Blockly.BBasic');
 
 
 export default (Blockly) => {
-  Blockly.BBasic['sprite_player0_get'] = function(block) {
-  // Variable getter.
-    const code = Blockly.BBasic.nameDB_.getName(block.getFieldValue('VAR'),
-        Blockly.VARIABLE_CATEGORY_NAME);
-    return [code, Blockly.BBasic.ORDER_ATOMIC];
-  };
+  const createGeneratorForSprite = (name) => {
+    Blockly.BBasic[`sprite_${name}_get`] = function(block) {
+      // Variable getter.
+      const code = Blockly.BBasic.nameDB_.getName(block.getFieldValue('VAR'),
+          Blockly.VARIABLE_CATEGORY_NAME);
+      return [code, Blockly.BBasic.ORDER_ATOMIC];
+    };
 
-  Blockly.BBasic['sprite_player0_set'] = function(block) {
-  // Variable setter.
-    const argument0 = Blockly.BBasic.valueToCode(block, 'VALUE',
-        Blockly.BBasic.ORDER_ASSIGNMENT) || '0';
-    const varName = Blockly.BBasic.nameDB_.getName(
-        block.getFieldValue('VAR'), Blockly.VARIABLE_CATEGORY_NAME);
-    return varName + ' = ' + argument0 + '\n';
-  };
+    Blockly.BBasic[`sprite_${name}_set`] = function(block) {
+      // Variable setter.
+      const argument0 = Blockly.BBasic.valueToCode(block, 'VALUE',
+          Blockly.BBasic.ORDER_ASSIGNMENT) || '0';
+      const varName = Blockly.BBasic.nameDB_.getName(
+          block.getFieldValue('VAR'), Blockly.VARIABLE_CATEGORY_NAME);
+      return varName + ' = ' + argument0 + '\n';
+    };
 
-  Blockly.BBasic['sprite_player0_change'] = function(block) {
+    Blockly.BBasic[`sprite_${name}_change`] = function(block) {
     // Add value do a variable.
-    const argument0 = Blockly.BBasic.valueToCode(block, 'DELTA',
-        Blockly.BBasic.ORDER_ASSIGNMENT) || '0';
-    const varName = Blockly.BBasic.nameDB_.getName(
-        block.getFieldValue('VAR'), Blockly.VARIABLE_CATEGORY_NAME);
-    return varName + ' = ' + varName + ' + ' + argument0 + '\n';
+      const argument0 = Blockly.BBasic.valueToCode(block, 'DELTA',
+          Blockly.BBasic.ORDER_ASSIGNMENT) || '0';
+      const varName = Blockly.BBasic.nameDB_.getName(
+          block.getFieldValue('VAR'), Blockly.VARIABLE_CATEGORY_NAME);
+      return varName + ' = ' + varName + ' + ' + argument0 + '\n';
+    };
   };
+
+  ['player0', 'player1', 'missile0', 'missile1', 'ball'].forEach(createGeneratorForSprite);
 };
