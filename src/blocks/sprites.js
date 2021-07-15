4ee54bef6558f56be67a2555f870b2d9c76b7735
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-07-15 19:23:08 -0300

    Generalize sprite block generation.

--- src/blocks/sprites.js
+++ src/blocks/sprites.js
@@ -1,72 +1,76 @@
 import * as Blockly from 'blockly/core';
 
-Blockly.defineBlocksWithJsonArray([ // BEGIN JSON EXTRACT
-  // Block for Player 0 getter.
-  {
-    'type': 'sprite_player0_get',
-    'message0': 'Player 0 %1',
-    'args0': [
-      {
-        'type': 'field_dropdown',
-        'name': 'VAR',
-        'options': [
-          ['X', 'player0x'],
-          ['Y', 'player0y'],
-          ['Color', 'player0color'],
-        ],
-      },
-    ],
-    'output': 'Number',
-    'colour': 'red',
-    'tooltip': 'Reads information about player zero',
-  },
-  // Block for Player 0 setter.
-  {
-    'type': 'sprite_player0_set',
-    'message0': 'Player 0: %{BKY_VARIABLES_SET}',
-    'args0': [
-      {
-        'type': 'field_dropdown',
-        'name': 'VAR',
-        'options': [
-          ['X', 'player0x'],
-          ['Y', 'player0y'],
-          ['Color', 'player0color'],
-        ],
-      },
-      {
-        'type': 'input_value',
-        'name': 'VALUE',
-      },
-    ],
-    'previousStatement': null,
-    'nextStatement': null,
-    'colour': 'red',
-    'tooltip': 'Updates information about player zero',
-  },
-  // Block for adding to a Player 0 variable in place.
-  {
-    'type': 'sprite_player0_change',
-    'message0': String.fromCodePoint(0x1F642) + ' Player 0: %{BKY_MATH_CHANGE_TITLE}',
-    'args0': [
-      {
-        'type': 'field_dropdown',
-        'name': 'VAR',
-        'options': [
-          ['\u2195 X', 'player0x'],
-          ['\u2195 Y', 'player0y'],
-          [String.fromCodePoint(0x1F3A8) + ' Color', 'player0color'],
-        ],
-      },
-      {
-        'type': 'input_value',
-        'name': 'DELTA',
-        'check': 'Number',
-      },
-    ],
-    'previousStatement': null,
-    'nextStatement': null,
-    'colour': 'red',
-    'extensions': ['math_change_tooltip'],
-  },
-]);
+const buildPlayerOptions = (name) => [
+  ['\u2195 X', `${name}x`],
+  ['\u2195 Y', `${name}y`],
+  [String.fromCodePoint(0x1F3A8) + ' Color', `${name}color`],
+];
+
+const buildSpriteBlocks = ({name, description, icon, options, colour}) => {
+  Blockly.defineBlocksWithJsonArray([
+    // Block for the getter.
+    {
+      'type': `sprite_${name}_get`,
+      'message0': `${icon} ${description} %1`,
+      'args0': [
+        {
+          'type': 'field_dropdown',
+          'name': 'VAR',
+          options,
+        },
+      ],
+      'output': 'Number',
+      colour,
+      'tooltip': `Reads information about ${description}`,
+    },
+    // Block for the setter.
+    {
+      'type': `sprite_${name}_set`,
+      'message0': `${icon} ${description}: %{BKY_VARIABLES_SET}`,
+      'args0': [
+        {
+          'type': 'field_dropdown',
+          'name': 'VAR',
+          options,
+        },
+        {
+          'type': 'input_value',
+          'name': 'VALUE',
+        },
+      ],
+      'previousStatement': null,
+      'nextStatement': null,
+      colour,
+      'tooltip': `Updates information about ${description}`,
+    },
+    // Block for adding to a variable in place.
+    {
+      'type': `sprite_${name}_change`,
+      'message0': `${icon} ${description}: %{BKY_MATH_CHANGE_TITLE}`,
+      'args0': [
+        {
+          'type': 'field_dropdown',
+          'name': 'VAR',
+          options,
+        },
+        {
+          'type': 'input_value',
+          'name': 'DELTA',
+          'check': 'Number',
+        },
+      ],
+      'previousStatement': null,
+      'nextStatement': null,
+      colour,
+      'extensions': ['math_change_tooltip'],
+    },
+  ]);
+};
+
+buildSpriteBlocks({
+  name: 'player0',
+  description: 'Player 0',
+  icon: String.fromCodePoint(0x1F642),
+  colour: 'red',
+  options: buildPlayerOptions('player0'),
+});
