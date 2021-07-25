9d786442b12c6201d222f3e1010590c181a9189c
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-07-25 14:18:11 -0300

    Route to the generated code.

---
+++ src/views/GeneratedCode.vue
@@ -0,0 +1,18 @@
+<template>
+    <p id="code">
+      <vue-code-highlight language="basic">
+        <pre v-html="code">Test!!!</pre>
+      </vue-code-highlight>
+    </p>
+</template>
+<script>
+import {defineComponent, reactive} from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'GeneratedCode',
+  setup() {
+    const code = reactive('Another test!!');
+    return {code};
+  },
+});
+</script>
