8086b349d2f13fd9aa32494cbdda170da8af6e53
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-07-25 14:52:06 -0300

    Remove unnecessary footer.

--- src/App.vue
+++ src/App.vue
@@ -139,22 +139,6 @@
     <v-main>
       <router-view/>
     </v-main>
-
-    <v-footer
-      app
-      color="transparent"
-      height="72"
-      inset
-    >
-      <v-text-field
-        background-color="grey lighten-1"
-        dense
-        flat
-        hide-details
-        rounded
-        solo
-      ></v-text-field>
-    </v-footer>
   </v-app>
 </template>
 
