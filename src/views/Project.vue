a1af41e592508fe92655f598544f67a48e60438a
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-07-25 18:42:48 -0300

    Create buttons.

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>
-    <h1>Project</h1>
-  </div>
+  <v-card>
+    <v-card-title>Project</v-card-title>
+    <v-card-actions>
+        <v-btn>Load project</v-btn>
+        <v-btn color="primary">Save project</v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 <script>
 import {defineComponent} from '@vue/composition-api';
