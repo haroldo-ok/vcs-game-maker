64c679f9c3d8cd2002eb80fc82582132da772d0a
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-07-25 20:59:56 -0300

    Implement project loading.

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,9 +1,15 @@
 <template>
   <v-card>
     <v-card-title>Project</v-card-title>
-    <v-card-text>{{ workspaceStorage }}</v-card-text>
+    <v-card-text>
+        <v-file-input
+            accept=".vcsgm"
+            label="Project to import."
+            v-model="data.fileToImport"
+            @change="handleLoadProject"
+        ></v-file-input>
+    </v-card-text>
     <v-card-actions>
-      <v-btn>Load project</v-btn>
       <v-btn
         color="primary"
         @click="handleSaveProject"
@@ -14,7 +20,7 @@
   </v-card>
 </template>
 <script>
-import {defineComponent} from '@vue/composition-api';
+import {defineComponent, reactive} from '@vue/composition-api';
 import {saveAs} from 'file-saver';
 import YAML from 'yaml';
 
@@ -25,11 +31,12 @@ const FORMAT_VERSION = 1.0;
 
 export default defineComponent({
   setup() {
+    const data = reactive({fileToImport: null});
     const workspaceStorage = useWorkspaceStorage();
-    return {workspaceStorage};
+    return {data, workspaceStorage};
   },
   methods: {
-    async handleSaveProject() {
+    handleSaveProject() {
       const projectYaml = YAML.stringify({
         'type': FORMAT_TYPE,
         'format-version': FORMAT_VERSION,
@@ -39,6 +46,35 @@ export default defineComponent({
 
       const projectBlob = new Blob([projectYaml], {type: 'text/yaml'});
       saveAs(projectBlob, 'project.vcsgm');
+    },
+
+    handleLoadProject() {
+      if (!this.data.fileToImport) {
+        console.warn('No file to import.');
+        return;
+      }
+
+      console.info('Importing file', this.data.fileToImport);
+      const reader = new FileReader();
+      reader.readAsText(this.data.fileToImport, 'UTF-8');
+      reader.onload = (evt) => {
+        const projectYaml = evt.target.result;
+        console.info('YAML', projectYaml);
+        const project = YAML.parse(projectYaml);
+
+        if (project.type !== FORMAT_TYPE) {
+          throw new Error('This file does not seem to be a valid project.');
+        }
+
+        if (project['format-version'] > FORMAT_VERSION) {
+          throw new Error(
+              `This project's version (${project['format-version']}) is newer than the supported version (${FORMAT_VERSION})`);
+        }
+
+        this.workspaceStorage = project['blockly-workspace'];
+      };
+      reader.onerror = (evt) => console.error('Error while loading project', evt);
+      this.data.fileToImport = null;
     },
   },
 });
