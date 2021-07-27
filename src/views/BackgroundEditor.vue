5d64f13c4850177964ace241c940a226995d4999
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-07-27 17:59:02 -0300

    Declare background editor view.

---
+++ src/views/BackgroundEditor.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-card>
+    <v-card-title>Backgrounds</v-card-title>
+    <v-card-text>TODO</v-card-text>
+  </v-card>
+</template>
+<script>
+import {defineComponent} from '@vue/composition-api';
+
+export default defineComponent({
+  setup() {
+  },
+});
+</script>
