5c2403ab12a0fc27112579986156aa5b8e09692c
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-07-27 18:31:27 -0300

    Remove blurring.

--- src/components/PixelEditor.vue
+++ src/components/PixelEditor.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <canvas ref="editor" />
+    <canvas ref="editor" class="editor-canvas" />
     <button @click="editor.tool = eraser">Eraser</button>
     <button @click="editor.tool = pencil">Pencil</button>
     <button @click="() => editor.undo()">Undo</button>
@@ -23,3 +23,13 @@ export default {
   },
 };
 </script>
+<style scoped>
+.editor-canvas {
+  image-rendering: optimizeSpeed;             /* Older versions of FF          */
+  image-rendering: -moz-crisp-edges;          /* FF 6.0+                       */
+  image-rendering: -webkit-optimize-contrast; /* Safari                        */
+  image-rendering: -o-crisp-edges;            /* OS X & Windows Opera (12.02+) */
+  image-rendering: pixelated;                 /* Awesome future-browsers       */
+  -ms-interpolation-mode: nearest-neighbor;   /* IE                            */
+}
+</style>
