7eaa04eef50fb258c9d0da7cde96dc3c831abafd
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-07-27 18:24:35 -0300

    Add `@curtishughes/pixel-editor`.

---
+++ src/components/PixelEditor.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <canvas ref="editor" />
+    <button @click="editor.tool = eraser">Eraser</button>
+    <button @click="editor.tool = pencil">Pencil</button>
+    <button @click="() => editor.undo()">Undo</button>
+    <button @click="() => editor.redo()">Redo</button>
+  </div>
+</template>
+<script>
+import {PixelEditor, Pencil} from '@curtishughes/pixel-editor';
+
+export default {
+  data() {
+    return {
+      pencil: new Pencil('black'),
+      eraser: new Pencil(),
+    };
+  },
+  mounted() {
+    const canvas = this.$refs.editor;
+    this.editor = new PixelEditor(canvas, 64, 64, this.pencil);
+  },
+};
+</script>
--- src/views/BackgroundEditor.vue
+++ src/views/BackgroundEditor.vue
@@ -1,13 +1,18 @@
 <template>
   <v-card>
     <v-card-title>Backgrounds</v-card-title>
-    <v-card-text>TODO</v-card-text>
+    <v-card-text>
+      <pixel-editor />
+    </v-card-text>
   </v-card>
 </template>
 <script>
 import {defineComponent} from '@vue/composition-api';
 
+import PixelEditor from '../components/PixelEditor.vue';
+
 export default defineComponent({
+  components: {PixelEditor},
   setup() {
   },
 });
