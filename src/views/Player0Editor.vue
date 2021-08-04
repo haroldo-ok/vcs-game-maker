f94355aae1d0b5f19c920cce5ecae762ecad01dc
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-08-04 19:12:05 -0300

    Mockup the sprite editor.

--- src/views/Player0Editor.vue
+++ src/views/Player0Editor.vue
@@ -1,11 +1,68 @@
 <template>
-  <h1>Player 0</h1>
+  <v-card>
+    <v-card-title>Player 0</v-card-title>
+    <v-card-text>
+      <v-list>
+        <v-list-item v-for="animation in animationData.animations" v-bind:key="animation.id">
+          <v-list-item-content>
+              <v-list-item-title>
+                <v-text-field label="Animation name" v-model="animation.name" />
+              </v-list-item-title>
+              <v-list>
+                <v-list-item v-for="frame in animation.frames" v-bind:key="frame.id">
+                  <div class="pixel-editor-container">
+                    <v-text-field
+                      label="Duration"
+                      v-model.number="frame.duration"
+                      hide-details
+                      type="number"
+                    />
+                    <pixel-editor
+                      :width="8"
+                      :height="8"
+                      :aspectRatio="160/192"
+                      v-model="frame.pixels"
+                      fgColor="red"
+                    />
+                  </div>
+                </v-list-item>
+              </v-list>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-card-text>
+  </v-card>
 </template>
 <script>
-import {defineComponent} from '@vue/composition-api';
+import {defineComponent, ref} from '@vue/composition-api';
+
+import PixelEditor from '../components/PixelEditor.vue';
 
 export default defineComponent({
+  components: {PixelEditor},
   setup() {
+    const animationData = ref({
+      animations: [
+        {
+          id: 1,
+          name: 'Example1',
+          frames: [
+            {
+              id: 1,
+              duration: 10,
+              pixels: null,
+            },
+            {
+              id: 2,
+              duration: 10,
+              pixels: null,
+            },
+          ],
+        },
+      ],
+    });
+
+    return {animationData};
   },
 });
 </script>
