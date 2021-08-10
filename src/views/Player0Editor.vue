7ffca5853a9ddbabd267f56b4bb1e6739747250d
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-08-09 22:02:20 -0300

    Create editor for player 1.

--- src/views/Player0Editor.vue
+++ src/views/Player0Editor.vue
@@ -1,7 +1,7 @@
 <template>
   <player-editor
     :storageFactory="usePlayer0Storage"
-    :title="'Player 0'"
+    title="Player 0"
     fgColor="red"
   />
 </template>
@@ -18,16 +18,3 @@ export default defineComponent({
   },
 });
 </script>
-<style scoped>
-.editor-container {
-  position: absolute;
-  overflow: auto;
-  top: 3em;
-  bottom: 0;
-  width: 100%;
-}
-
-.pixel-editor-parent-container {
-  display: inline-block;
-}
-</style>
