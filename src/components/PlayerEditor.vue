50f1ce2ec5bf5be9dfa3528cf9d1c4941577dd84
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-08-18 18:50:35 -0300

    Create button for adding animation frame.
    Does nothing, for now.

--- src/components/PlayerEditor.vue
+++ src/components/PlayerEditor.vue
@@ -1,42 +1,57 @@
 <template>
-  <v-card class="editor-container">
-    <v-card-title>{{ title }}</v-card-title>
-    <v-card-text>
-      <v-list>
-        <v-list-item v-for="animation in state.animations" v-bind:key="animation.id">
-          <v-list-item-content>
-              <v-list-item-title>
-                <v-text-field label="Animation name" v-model="animation.name" />
-              </v-list-item-title>
-              <v-list>
-                <v-list-item
-                  v-for="frame in animation.frames"
-                  v-bind:key="frame.id"
-                  class="pixel-editor-parent-container"
-                >
-                  <div class="pixel-editor-container">
-                    <v-text-field
-                      label="Duration"
-                      v-model.number="frame.duration"
-                      hide-details
-                      type="number"
-                    />
-                    <pixel-editor
-                      :width="8"
-                      :height="8"
-                      :aspectRatio="160/192"
-                      v-model="frame.pixels"
-                      :fgColor="fgColor"
-                      @input="handleChildChange"
-                    />
-                  </div>
-                </v-list-item>
-              </v-list>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-card-text>
-  </v-card>
+  <div>
+    <v-card class="editor-container">
+      <v-card-title>{{ title }}</v-card-title>
+      <v-card-text>
+        <v-list>
+          <v-list-item v-for="animation in state.animations" v-bind:key="animation.id">
+            <v-list-item-content>
+                <v-list-item-title>
+                  <v-text-field label="Animation name" v-model="animation.name" />
+                </v-list-item-title>
+                <v-list>
+                  <v-list-item
+                    v-for="frame in animation.frames"
+                    v-bind:key="frame.id"
+                    class="pixel-editor-parent-container"
+                  >
+                    <div class="pixel-editor-container">
+                      <v-text-field
+                        label="Duration"
+                        v-model.number="frame.duration"
+                        hide-details
+                        type="number"
+                      />
+                      <pixel-editor
+                        :width="8"
+                        :height="8"
+                        :aspectRatio="160/192"
+                        v-model="frame.pixels"
+                        :fgColor="fgColor"
+                        @input="handleChildChange"
+                      />
+                    </div>
+                  </v-list-item>
+                </v-list>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-card-text>
+    </v-card>
+
+
+    <v-btn
+      class="add-frame-buttom"
+      color="primary"
+      title="Add animation frame"
+      dark
+      absolute
+      right
+      fab
+    >
+      <v-icon>mdi-plus</v-icon>
+    </v-btn>
+  </div>
 </template>
 <script>
 import {computed, defineComponent} from '@vue/composition-api';
@@ -120,5 +135,9 @@ export default defineComponent({
 
 .pixel-editor-parent-container {
   display: inline-block;
+}
+
+.add-frame-buttom {
+  bottom: 8px;
 }
 </style>
