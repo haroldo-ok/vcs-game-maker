00e8b5caef6573bbcb6b6e59c1962ef25fd4cb95
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2024-04-13 11:18:47 -0300

    Configure `fs` webpack pollyfill.

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,7 @@ module.exports = {
 			"crypto": require.resolve("crypto-browserify"),
 			"stream": require.resolve("stream-browserify"),
 			"assert": require.resolve("assert"),
-			"fs": require.resolve("fs"),
+			"fs": require.resolve("browserify-fs"),
 			"http": require.resolve("stream-http"),
 			"https": require.resolve("https-browserify"),
 			"os": require.resolve("os-browserify"),
