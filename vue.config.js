4339e0f588f7c9437601f1ef379d6744d28243f7
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2024-04-13 11:10:34 -0300

    Fix webpack polyfills.
    Except `fs`; that one is still broken.

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,19 @@ module.exports = {
     'vuetify',
   ],
   publicPath: './',
+  configureWebpack: {
+	  resolve: {
+		fallback: {
+			"crypto": require.resolve("crypto-browserify"),
+			"stream": require.resolve("stream-browserify"),
+			"assert": require.resolve("assert"),
+			"fs": require.resolve("fs"),
+			"http": require.resolve("stream-http"),
+			"https": require.resolve("https-browserify"),
+			"os": require.resolve("os-browserify"),
+			"path": require.resolve("path-browserify"),
+			"url": require.resolve("url")
+		},
+	  },
+  },
 };
