c83b29411bfdcc11d019b202bf8f90fd6df5e4df
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2024-05-21 18:00:45 -0300

    Generate code for setting audio registers

--- src/generators/bbasic/sound.js
+++ src/generators/bbasic/sound.js
@@ -2,7 +2,14 @@
 
 export default (Blockly) => {
   Blockly.BBasic[`simple_sound_set`] = function(block) {
-    const code = `AUDC0=1\nAUDV0=15\nAUDF0=31`;
+    const audc = block.getFieldValue('AUDC');
+    const audf = block.getFieldValue('AUDF');
+    const audv = block.getFieldValue('AUDV');
+    const channel = block.getFieldValue('CHANNEL');
+    const code = `AUDV${channel}=0\n` +
+      `AUDC${channel}=${audc}\n` +
+      `AUDF${channel}=${audf}\n` +
+      `AUDV${channel}=${audv}\n`;
     return code;
   };
 };
