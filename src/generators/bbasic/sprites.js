5dc9f262d43f95c9f17983ab953b08cd94abea89
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2024-06-02 16:40:16 -0300

    Implement generator for missile width.

--- src/generators/bbasic/sprites.js
+++ src/generators/bbasic/sprites.js
@@ -80,12 +80,22 @@ export default (Blockly) => {
 
   const createGeneratorForPlayer = (name) => {
     Blockly.BBasic[`sprite_${name}_size`] = function(block) {
-      const size = block.getFieldValue('SIZE');
+      const size = block.getFieldValue('SIZE') || '0';
       return `${name}size = ${name}size & $F0\n` +
         `${name}size = ${name}size | ${size}\n`;
     };
   };
 
+  const createGeneratorForMissile = (name) => {
+    Blockly.BBasic[`sprite_${name}_size`] = function(block) {
+      const size = block.getFieldValue('SIZE') || 0;
+      const varName = name.replace('missile', 'player') + 'size';
+      return `${varName} = ${varName} & $0F\n` +
+        `${varName} = ${varName} | ${size}\n`;
+    };
+  };
+
   ['player0', 'player1', 'missile0', 'missile1', 'ball'].forEach(createGeneratorForSprite);
   ['player0', 'player1'].forEach(createGeneratorForPlayer);
+  ['missile0', 'missile1'].forEach(createGeneratorForMissile);
 };
