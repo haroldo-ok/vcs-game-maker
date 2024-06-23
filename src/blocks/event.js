de00f478ddeffa1161c2c346c547dbf282685896
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2024-06-23 20:03:07 -0300

    Add event options

--- src/blocks/event.js
+++ src/blocks/event.js
@@ -1,10 +1,21 @@
 import * as Blockly from 'blockly/core';
 
+const EVENT_OPTIONS = [
+  ['System start', `system_start`],
+];
+
 Blockly.defineBlocksWithJsonArray([
   // Block for startup code
   {
     'type': 'event_block',
-    'message0': 'On',
+    'message0': 'On %1',
+    'args0': [
+      {
+        'type': 'field_dropdown',
+        'name': 'EVENT',
+        'options': EVENT_OPTIONS,
+      },
+    ],
     'message1': '%1',
     'args1': [{
       'type': 'input_statement',
