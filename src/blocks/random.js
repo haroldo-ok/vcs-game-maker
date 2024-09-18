3861a09c0c16ef4a74cc52ab241f6955be05b429
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2024-09-18 20:57:04 -0300

    Add more options.
    Based on the table on https://www.randomterrain.com/atari-2600-memories-batari-basic-commands.html#rand

--- src/blocks/random.js
+++ src/blocks/random.js
@@ -3,8 +3,20 @@ import * as Blockly from 'blockly/core';
 import {DICE_ICON} from './icon';
 
 const RAND_OPTIONS = [
+  ['0 to 1', '(rand/128)'],
+  ['1 to 2', '(rand/128) + 1'],
+  ['0 to 3', '(rand/64)'],
+  ['1 to 4', '(rand/64) + 1'],
+  ['0 to 7', '(rand/32)'],
+  ['1 to 8', '(rand/32) + 1'],
+  ['0 to 15', '(rand/16)'],
+  ['1 to 16', '(rand/16) + 1'],
+  ['0 to 31', '(rand/8)'],
   ['1 to 32', '(rand/8) + 1'],
   ['0 to 63', '(rand/4)'],
+  ['1 to 64', '(rand/4) + 1'],
+  ['0 to 127', '(rand/2)'],
+  ['1 to 128', '(rand/2) + 1'],
 ];
 
 Blockly.defineBlocksWithJsonArray([
