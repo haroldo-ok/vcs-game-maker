5d5ba5af3f5f49b7849dc5dcce38d87993dc8606
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2024-09-18 19:35:22 -0300

    Create the `random_range_get` block.
    Starting #62 and #95

--- src/blocks/random.js
+++ src/blocks/random.js
@@ -14,3 +14,27 @@ Blockly.defineBlocksWithJsonArray([
     'tooltip': `Generates a random number between 1 and 255.`,
   },
 ]);
+
+Blockly.defineBlocksWithJsonArray([
+  // Block for the getter.
+  {
+    'type': `random_range_get`,
+    'message0': `Random from %1 to %2`,
+    'args0': [
+      {
+        'type': 'field_number',
+        'name': 'RANGE_START',
+        'value': 1,
+      },
+      {
+        'type': 'field_number',
+        'name': 'RANGE_START',
+        'value': 32,
+      },
+    ],
+    'output': 'Number',
+    'icon': DICE_ICON,
+    'colour': 'purple',
+    'tooltip': `Generates a random number between the given numbers.`,
+  },
+]);
