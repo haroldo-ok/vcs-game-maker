00989f1990c5e4a55a6cc16c06c8458673c0e76c
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2024-10-28 19:58:36 -0300

    Create a block to do something every few frames.

--- src/blocks/event.js
+++ src/blocks/event.js
@@ -16,6 +16,10 @@ const STATE_OPTIONS = [
   [`${GAMEOVER_ICON} Gameover`, 'gameover'],
 ];
 
+const FRAME_OPTIONS = [...Array(8).keys()]
+    .map((n) => Math.pow(2, n))
+    .map((n) => [`${n}`, `${n - 1}`]);
+
 Blockly.defineBlocksWithJsonArray([
   // Block for events
   {
@@ -75,6 +79,33 @@ Blockly.defineBlocksWithJsonArray([
     'nextStatement': null,
     'colour': 'rgb(39, 176, 176)',
     'tooltip': 'Change game state to a given one',
+  },
+
+  // Block for executing every "n" frames
+  {
+    'type': 'event_frame_every_n',
+    'message0': 'Every %1 frames, plus %2',
+    'args0': [
+      {
+        'type': 'field_dropdown',
+        'name': 'MASK',
+        'options': FRAME_OPTIONS,
+      },
+      {
+        'type': 'field_number',
+        'name': 'DELTA',
+        'value': 0,
+      },
+    ],
+    'message1': '%1',
+    'args1': [{
+      'type': 'input_statement',
+      'name': 'DO',
+    }],
+    'previousStatement': null,
+    'nextStatement': null,
+    'colour': 'rgb(39, 176, 176)',
+    'tooltip': 'Execute code every few frames',
   },
 
 ]);
