8144ba9e87ff36182733dd5f15d5a584a734b9e4
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2025-08-02 07:56:52 -0300

    Fix error display.

--- src/App.vue
+++ src/App.vue
@@ -176,7 +176,7 @@
     </v-main>
 
     <v-footer class="error-message">
-      <pre v-text="errorStorage"></pre>
+      <pre v-text="errorStorage.value"></pre>
     </v-footer>
   </v-app>
 </template>
