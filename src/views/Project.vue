a340e809995d252284b8167f732252e6a952fa9f
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2025-10-22 06:58:32 -0300

    Create a dialog that opens when clicking the button.
    Continuing #142

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -16,12 +16,47 @@
       >
           Save Project
       </v-btn>
-      <v-btn
-        color="secondary"
-        @click="handleSaveProject"
-      >
-          Create New Project
-      </v-btn>
+      <template>
+        <div class="text-center">
+          <v-dialog
+            v-model="dialog"
+            width="500"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                color="secondary"
+                v-bind="attrs"
+                v-on="on"
+              >
+                Create New Project
+              </v-btn>
+            </template>
+
+            <v-card>
+              <v-card-title class="text-h5 grey lighten-2">
+                Privacy Policy
+              </v-card-title>
+
+              <v-card-text>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </v-card-text>
+
+              <v-divider></v-divider>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  color="primary"
+                  text
+                  @click="dialog = false"
+                >
+                  I accept
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </div>
+      </template>
     </v-card-actions>
   </v-card>
 </template>
